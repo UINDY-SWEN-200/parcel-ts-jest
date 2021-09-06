fe6a14d94facdfacafa1a4a18e0fd579d1dad1d0
Steve Spicklemire <steve@spvi.com>
2021-09-06 10:01:04 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,36 @@ export function attachClickHandler(elID: string, handler: (e: MouseEvent) => voi
     }
 }
 
+export function checkDates(start: Date, end: Date): boolean {
+    //
+    // Utility function that checks if a start date is before an end date
+    //
+    return start.getTime() <= end.getTime()
+}
+
+export function handleDialogClick(e: MouseEvent) {
+    //
+    // Event handler for the button
+    //
+    e.preventDefault()
+    console.log("submit -> " + new Date().toISOString())
+
+    const startDateInput = document.getElementById("start") as HTMLInputElement
+    const endDateInput = document.getElementById("end") as HTMLInputElement
+
+    if (startDateInput && endDateInput) {
+        const start = new Date(startDateInput.value)
+        const end = new Date(endDateInput.value)
+        const datesOK = checkDates(start, end)
+        console.log("Found two dates:" + datesOK)
+    } else {
+        console.log("No dates inputs found")
+        throw new Error("No dates inputs found")
+    }
+
+    
+}
+
 const today =  new Date()
 
 export function renderEventDialog( begin: Date = today, end: Date = today, title: string = "A new event", description: string = "") {
@@ -29,16 +59,16 @@ export function renderEventDialog( begin: Date = today, end: Date = today, title
     <form action="#" id="event-form">
     <h1> Create Event Dialog</h1>
     <h2>Title: <input type="text" id="title" name="event-title" value="${title}"/></h2>
-    Start Date: <input type="date" id="start" name="event-start"
-    value="${begin.toISOString().slice(0, 10)}"/>
+    <label for="start">Start Date:</label>
+    <input type="date" id="start" name="event-start" value="${begin.toISOString().slice(0, 10)}"/>
     <br/>
     <br/>
-    End Date: <input type="date" id="end" name="event-end"
-    value="${end.toISOString().slice(0, 10)}"/>
+    <label for="end">End Date: </label>
+    <input type="date" id="end" name="event-end" value="${end.toISOString().slice(0, 10)}"/>
     <br/>
     <br/>
-    Description: <input type="text" id="description" name="event-description"
-    value="${description}"/>
+    <label for="description">Description:</label>
+    <input type="text" id="description" name="event-description"  value="${description}"/>
     <br/>
     <br/>
     <input type="submit" value="Create Event" id="add-event"/>
@@ -47,9 +77,9 @@ export function renderEventDialog( begin: Date = today, end: Date = today, title
     return html
 }
 
-applyUI("app", renderEventDialog())
+export function buildUI() {
+    applyUI("app", renderEventDialog())
+    attachClickHandler("add-event",handleDialogClick)
+}
 
-attachClickHandler("add-event",(e: MouseEvent) => {
-    e.preventDefault()
-    console.log("clicked" + new Date())
-})
+buildUI()
