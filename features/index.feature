3f3cf8c1174510d01c1efb20af2a40cf982fb579
Steve Spicklemire <steve@spvi.com>
2021-09-08 20:20:57 -0400

    add invalid data check

--- features/index.feature
+++ features/index.feature
@@ -4,3 +4,8 @@ Feature: Submitting Event
         Given I desire to create an event
         When I enter valid data
         Then The event should be created and added to the db
+
+    Scenario: Submitting a bad Event
+        Given I desire to create an event
+        When I enter invalid data
+        Then The event should fail to be be created
--- steps/index.steps.ts
+++ steps/index.steps.ts
@@ -11,25 +11,33 @@ import {
 
 const feature = loadFeature("features/index.feature")
 
+function setupUpUI(aDb: DumbDB, errors: string[] = []) {
+  const div = document.createElement("div")
+  div.innerHTML = `<div id="app"></div>`
+  document.body.appendChild(div) // add the div to the DOM
+  const html = renderEventDialog()
+  applyUI("app", html)
+  attachClickHandler("add-event", (e) => {
+    e.preventDefault()
+    try {
+      storeDialogData(aDb)
+    } catch (e) {
+      errors.push((e as Error).message)
+    }
+  })
+}
+
 defineFeature(feature, (test) => {
   test("Submitting a good Event", ({ given, when, then }) => {
     const aDb = new DumbDB()
 
     given("I desire to create an event", () => {
       // mock loading web page
-      const div = document.createElement("div")
-      div.innerHTML = `<div id="app"></div>`
-      document.body.appendChild(div) // add the div to the DOM
-      const html = renderEventDialog()
-      applyUI("app", html)
-      attachClickHandler("add-event", (e) => {
-        e.preventDefault()
-        storeDialogData(aDb)
-      })
+      setupUpUI(aDb)
     })
 
     when("I enter valid data", () => {
-      // possibly modify form elements to match valid data
+      // data is valid by default
       let button = document.getElementById("add-event")
       if (button) {
         button.click()
@@ -40,6 +48,30 @@ defineFeature(feature, (test) => {
       expect(aDb.length).toBe(1)
     })
   })
-})
 
-test("check for UI handling", () => {})
+  test("Submitting a bad Event", ({ given, when, then }) => {
+    const aDb = new DumbDB()
+    const errors: string[] = []
+
+    given("I desire to create an event", () => {
+      // mock loading web page
+      setupUpUI(aDb, errors)
+    })
+
+    when("I enter invalid data", () => {
+      let startTimeInput = document.getElementById("start") as HTMLInputElement
+      let endTimeInput = document.getElementById("end") as HTMLInputElement
+      startTimeInput.value = "2021-01-01"
+      endTimeInput.value = "2020-01-01" // bad data
+      let button = document.getElementById("add-event")
+      if (button) {
+        button.click()
+      }
+    })
+
+    then("The event should fail to be be created", () => {
+      expect(aDb.length).toBe(0)
+      expect(errors.length).toBe(1)
+    })
+  })
+})
